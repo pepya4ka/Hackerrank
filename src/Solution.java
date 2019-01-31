82c5512866e8e98ec90e079afab27a0798784cc6
Nikita <ndrozdov9@gmail.com>
2019-02-01 01:32:10 +0500

    Java End-of-file

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,21 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.*;
 
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int i = 0;
-        while (sc.hasNext()) {
-            String s = sc.nextLine();
-            System.out.println(++i + " " + s);
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                sum += + Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
         }
+        in.close();
     }
 }
