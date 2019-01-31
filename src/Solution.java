b0b5cb9a826d8612e37958e65dc53ba7f90d2b62
Nikita <ndrozdov9@gmail.com>
2019-02-01 01:37:38 +0500

    Java End-of-file

--- src/Solution.java
+++ src/Solution.java
@@ -1,68 +1,17 @@
-import java.util.*;
-
-class Student {
-    private int id;
-    private String fname;
-    private double cgpa;
-
-    public Student(int id, String fname, double cgpa) {
-        super();
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
-}
-
-//Complete the code
-
-class Checker implements Comparator<Student> {
-
-    @Override
-    public int compare(Student o1, Student o2) {
-        if (o1.getCgpa() < o2.getCgpa()) {
-            return -1;
-        } else {
-            if (o1.getCgpa() == o2.getCgpa()) {
-                return o1.getFname().compareTo(o2.getFname());
-            } else {
-                return 1;
-            }
-        }
-    }
-}
+import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        Checker checker = new Checker();
-        List<Student> studentList = new ArrayList<Student>();
-        while (testCases > 0) {
-            int id = in.nextInt();
-            String fname = in.next();
-            double cgpa = in.nextDouble();
-
-            Student st = new Student(id, fname, cgpa);
-            studentList.add(st);
-
-            testCases--;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
         }
-        studentList.sort(checker);
+        System.out.println("================================");
 
-        for (Student st : studentList) {
-            System.out.println(st.getFname());
-        }
     }
 }
