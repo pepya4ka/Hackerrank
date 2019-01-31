761ce55588955b660c1320ed3551e9222fad2ffe
Nikita <ndrozdov9@gmail.com>
2019-02-01 01:49:49 +0500

    Java End-of-file

--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,30 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-import static java.util.Calendar.*;
-class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
-    public static String findDay(int month, int day, int year) {
-        Calendar c = new GregorianCalendar(year, month - 1, day);
-        int i = c.get(Calendar.DAY_OF_WEEK);
-        switch (i) {
-            case SUNDAY: return "SUNDAY";
-            case MONDAY: return "MONDAY";
-            case TUESDAY: return "TUESDAY";
-            case WEDNESDAY: return "WEDNESDAY";
-            case THURSDAY: return "THURSDAY";
-            case FRIDAY: return "FRIDAY";
-            case SATURDAY: return "SATURDAY";
-            default: return " ";
-        }
-    }
-
-}
+import java.text.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
 
-        int month = Integer.parseInt(firstMultipleInput[0]);
+        // Write your code here.
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = nf.format(payment);
 
-        int day = Integer.parseInt(firstMultipleInput[1]);
+        nf = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        String india = nf.format(payment);
 
-        int year = Integer.parseInt(firstMultipleInput[2]);
+        nf = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = nf.format(payment);
 
-        String res = Result.findDay(month, day, year);
+        nf = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = nf.format(payment);
 
-        bufferedWriter.write(res);
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
 }
+
