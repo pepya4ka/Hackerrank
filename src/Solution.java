2a1a1c6ac12f53842b75833a5ac646c81f34ddf9
Nikita <ndrozdov9@gmail.com>
2019-02-01 01:26:01 +0500

    Java End-of-file

--- src/Solution.java
+++ src/Solution.java
@@ -1,71 +1,18 @@
+import java.io.*;
 import java.util.*;
-
-class Student {
-    private int id;
-    private String fname;
-    private double cgpa;
-
-    public Student(int id, String fname, double cgpa) {
-        super();
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
-}
-
-//Complete the code
-
-class Checker implements Comparator<Student> {
-
-    @Override
-    public int compare(Student o1, Student o2) {
-        if (o1.getCgpa() < o2.getCgpa()) {
-            return -1;
-        } else {
-            if (o1.getCgpa() == o2.getCgpa()) {
-                return o1.getFname().compareTo(o2.getFname());
-            } else {
-                return 1;
-            }
-        }
-    }
-}
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        Checker checker = new Checker();
-        List<Student> studentList = new ArrayList<Student>();
-        while (testCases > 0) {
-            int id = in.nextInt();
-            String fname = in.next();
-            double cgpa = in.nextDouble();
-
-            Student st = new Student(id, fname, cgpa);
-            studentList.add(st);
-
-            testCases--;
-        }
-        studentList.sort(checker);
-
-        for (Student st : studentList) {
-            System.out.println(st.getFname());
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            System.out.println(++i + " " + s);
         }
     }
 }
-
-
-
