ac6c41c2aaf88e001e339956cc4d64a7306b8665
Nikita <ndrozdov9@gmail.com>
2019-02-01 01:46:45 +0500

    Java End-of-file

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,48 @@
 import java.util.*;
-import java.io.*;
+import java.security.*;
+public class Solution {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+            //String s=???; Complete this line below
+
+            //Write your code here
+            String s = Integer.toString(n);
 
 
-class Solution {
-    public static void main(String[] argh) {
-
-
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        for (int i = 0; i < t; i++) {
-
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
-                if (x >= -128 && x <= 127) System.out.println("* byte");
-                //Complete the code
-                if (x >= -32768 && x <= 32767) System.out.println("* short");
-                if (x >= -2147483648 && x <= 2147483647) System.out.println("* int");
-                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
-                else System.out.println("can't be fitted anywhere.");
-            } catch (Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
             }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
         }
     }
 }
 
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
 
+    public static class ExitTrappedException extends SecurityException {
 
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
